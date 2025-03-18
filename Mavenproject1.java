250bde914e1e62db9cf825ff58345a79972e1e35
Mateus Nunes <jmateus12326609@gmail.com>
2025-03-18 20:47:11 -0300

    Add files via upload

---
+++ Mavenproject1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author teus
+ */
+import java.util.Scanner;
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        
+        Scanner tecla = new Scanner(System.in);
+        System.out.println("Digite a quantidade de dolar que você quer converter em reais.");
+        double dolar = tecla.nextDouble();
+        System.out.println(dolar * 5.88);
+        double reais = tecla.nextDouble();
+        reais = tecla.nextDouble();
+        System.out.printf("em reais o valor é", reais);
+    }
+}
