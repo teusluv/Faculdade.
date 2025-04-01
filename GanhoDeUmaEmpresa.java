a7f69e7e3ea320752eda73b4908a24eece8bca17
Mateus Nunes <jmateus12326609@gmail.com>
2025-04-01 11:21:40 -0300

    Add files via upload

---
+++ GanhoDeUmaEmpresa.java
@@ -0,0 +1,14 @@
+
+public class GanhoDeUmaEmpresa {
+    public static void main(String[] args) {
+        double n1, n2, n3, n4, media;
+        n1 = 3459.90;
+        n2 = 1434.70;
+        n3 = 12133.32;
+        n4 = 343443.63;
+        media = (n1 + n2 + n3 + n4) / 4;
+        System.out.println("A media de valores ganho é:" + media);
+
+    }
+
+}
